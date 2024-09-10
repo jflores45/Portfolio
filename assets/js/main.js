b6b856bbfe07d5e05f79ee10f27e7f4b3a491d36
jflores45 <jahaira@umich.edu>
2024-09-10 12:36:04 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/LO4GOT35QKcc0nQR/scene.splinecode');
